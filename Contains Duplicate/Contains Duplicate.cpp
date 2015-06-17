8105451538b7020c51a6ccb4119a468394e4c707
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-17 20:38:45 +0800

    init commit

---
+++ Contains Duplicate/Contains Duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] == nums[i-1])    return true;
+        }
+        return false;
+    }
+};
