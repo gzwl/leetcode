ac760f79693c46f41456d14f83eecf0dab68b342
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-17 20:04:23 +0800

    init commit

---
+++ InvertBinaryTree/InvertBinaryTree.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)    return  root;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left,root->right);
+        return root;
+    }
+};
