af78e0ec2e4f3ae037760fc0989155a9fccb48f0
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-18 12:20:09 +0800

    initial commit

---
+++ Excel Sheet Column Number/Excel Sheet Column Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int n = s.length();
+        int res = 0,bit = 1;
+        for(int i = n-1; i >= 0; i--)
+        {
+            res += (s[i] - 'A' + 1) * bit;
+            bit *= 26;
+        }
+        return res;
+    }
+};
---
+++ Excel Sheet Column Title/Excel Sheet Column Title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string s;
+        while(n)
+        {
+            int t = n-1;
+            t %= 26;
+            char c = t + 'A';
+            s += c;
+            n = (n-1)/26;
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
