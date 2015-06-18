2586bcc367a5630aa55108911552865133322983
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-18 19:31:34 +0800

    Update Number of 1 Bits.cpp

--- Number of 1 Bits/Number of 1 Bits.cpp
+++ Number of 1 Bits/Number of 1 Bits.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int res=0;
+        int res = 0;
         while(n){
-            n&=(n-1);
+            n &= (n - 1);
             res++;
         }
         return res;
