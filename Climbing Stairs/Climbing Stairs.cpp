72134c13213177a5fbc39afafa900094ca9e6dd3
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-18 12:38:16 +0800

    initial commit

---
+++ Climbing Stairs/Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int *dp = new int[n+1];     //dp[i] means the distinct ways at ith stair
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        int res = dp[n];
+        delete [] dp;
+        return res;
+    }
+};
---
+++ Count and Say/Count and Say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s[2];
+        int cur = 0;
+        s[cur] = "1";
+        for(int i = 2;i <= n; i++){
+            s[cur^1] = "";
+            for(int j = 0;j<s[cur].size();j++){
+                int cnt = 0;
+                int k;
+                for( k=j ; j<s[cur].size() && s[cur][j] == s[cur][k] ; k++)     cnt++;
+                j = k-1;
+                s[cur^1] += static_cast<char>('0'+cnt);
+                s[cur^1] += s[cur][j];
+            }
+            cur ^= 1;
+        }
+        return s[cur];
+    }
+};
