73fa4e1804e6752c56a68365e18b317e20f7acb9
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-19 13:40:16 +0800

    Update Contains Duplicate.cpp

--- Contains Duplicate/Contains Duplicate.cpp
+++ Contains Duplicate/Contains Duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
+        for(int i = 1;i < nums.size();i++){
             if(nums[i] == nums[i-1])    return true;
         }
         return false;
