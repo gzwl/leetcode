add56341a5ffa4e01d154592506ce24e6189634b
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-21 17:21:44 +0800

    initial commit

--- Interleaving String/Interleaving String.cpp
+++ Interleaving String/Interleaving String.cpp
@@ -6,9 +6,9 @@ public:
         for(int i = 0;i <= s1.size();i++){
             dp[i] = new int[s2.size()+1];
             for(int j = 0;j <= s2.size();j++){
-                dp[i][j] = 0;
+                dp[i][j] = 0;  
             }
-        }
+        }               //dp[i][j]表示s1的前i个字符和s2的前j个字符能否构成s3的前i+j个字符，然后状态转移就可以了
         dp[0][0] = 1;
         for(int len = 1;len <= s3.size();len++){
             for(int len1 = max(0,(int)(len - s2.size()));len1 <= min(len,(int)s1.size());len1++){
