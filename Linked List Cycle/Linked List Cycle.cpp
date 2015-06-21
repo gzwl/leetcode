2eb1d69fdf31e60f7181919c9e3f15d64c39a0e1
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-21 14:36:29 +0800

    initial commit

--- Linked List Cycle/Linked List Cycle.cpp
+++ Linked List Cycle/Linked List Cycle.cpp
@@ -9,9 +9,9 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *p1 = head,*p2 = head;
+        ListNode *p1 = head,*p2 = head;     //快慢指针
         while(p1 && p2){
-            if(p1->next && p1->next->next)  p1 = p1->next->next;
+            if(p1->next && p1->next->next)  p1 = p1->next->next;    //一个速度是另一个的两倍，如果有圈一定会再次相遇    
             else    return false;
             p2 = p2->next;
             if(p1 == p2)    return true;
