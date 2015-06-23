6686f4811b2dd76d3d794842cc4f69b97748615a
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-23 14:11:12 +0800

    位运算 想法

---
+++ Bitwise AND of Numbers Range/Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,18 @@
+/*
+    仔细想想我们只要找m和n的最高不同位bit就可以了
+    因为n和m之间一定会出现一个0~bit-1位都是0的数
+    那么答案就是bit位以下的都是0，bit位以上的和m,n一样
+*/
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int bit = 0;
+        while(m != n){
+            m = m >> 1;
+            n = n >> 1;
+            bit++;
+        }
+        return m << bit;
+    }
+};
