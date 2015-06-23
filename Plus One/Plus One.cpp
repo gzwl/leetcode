1a771deaff46b3877fb7bb08fc8e9738a577fd6e
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-23 15:42:17 +0800

    简单模拟

--- Plus One/Plus One.cpp
+++ Plus One/Plus One.cpp
@@ -1,13 +1,18 @@
+/*
+    解法很直接了，就是模拟手算
+    不过要注意进位
+*/
+
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
         int n = digits.size();
         reverse(digits.begin(),digits.end());
-        int in = 1;
+        int in = 1;     
         for(int i = 0; i < n && in; i++){
             digits[i] += in;
             in = 0;
-            if(digits[i] > 9 ){
+            if(digits[i] > 9 ){     //进位
                 digits[i] = 0;
                 in = 1;
             }
