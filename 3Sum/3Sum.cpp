4186e59300a8fba89164acce14e6f8d1316316a8
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-23 15:20:10 +0800

    排序

--- 3Sum/3Sum.cpp
+++ 3Sum/3Sum.cpp
@@ -1,4 +1,9 @@
-//time complexity O(n^2)
+/*
+    假设三个数  a <= b <= c 
+    为了求a+b+c == target 那么排完序后我们先O(n)枚举a
+    然后对于每个a我们再O(n)枚举a右边的数
+    最后总的时间复杂度O(n^2)
+*/
 
 class Solution {
 public:
@@ -7,12 +12,12 @@ public:
         vector<vector<int> > res;
         int n = num.size();
         if(n < 3)   return res;
-        for(int i = 0; i < n; i++){
-            if(i && num[i] == num[i-1])     continue;
-            int lhs = i+1,rhs = n-1;
-            while(rhs > lhs){
-                 if(rhs != n-1 && num[rhs] == num[rhs+1])   rhs--;
-                 else if(lhs != i+1 && num[lhs] == num[lhs-1])      lhs++;
+        for(int i = 0; i < n; i++){                             //枚举a
+            if(i && num[i] == num[i-1])     continue;           //排除重复的解
+            int lhs = i+1,rhs = n-1;                            //b和c的下标
+            while(rhs > lhs){                                   //O(n)枚举b和c
+                 if(rhs != n-1 && num[rhs] == num[rhs+1])   rhs--;              //排除重复的解
+                 else if(lhs != i+1 && num[lhs] == num[lhs-1])      lhs++;      //排除重复的解
                  else if(num[i] + num[lhs] + num[rhs] > 0)   rhs--;
                  else if(num[i] + num[lhs] + num[rhs] < 0)   lhs++;
                  else{
