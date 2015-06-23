0333b11e07e046795e3572e6ad40e19f33ccbdc0
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-23 15:20:14 +0800

    排序

--- 3Sum Closest/3Sum Closest.cpp
+++ 3Sum Closest/3Sum Closest.cpp
@@ -1,14 +1,19 @@
-//time complexity O(n^2)
+/*
+    假设三个数  a <= b <= c 
+    为了求a+b+c和target最接近 那么排完序后我们先O(n)枚举a
+    然后对于每个a我们再O(n)枚举a右边的数
+    最后总的时间复杂度O(n^2)
+*/
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int res = nums[0] + nums[1] + nums[2];
         int n = nums.size();
-        for(int i = 0; i < n && res != target; i++){
-            if(i && nums[i] == nums[i-1])     continue;
-            int lhs = i+1,rhs = n-1;
-            while(rhs > lhs){
+        for(int i = 0; i < n && res != target; i++){    //枚举a
+            if(i && nums[i] == nums[i-1])     continue;     //排除重复的解
+            int lhs = i+1,rhs = n-1;        //b和c的下标
+            while(rhs > lhs){               //O(n)枚举b和c
                  int tmp = nums[i] + nums[lhs] + nums[rhs];
                  if(abs(tmp - target) < abs(res - target)){
                      res = tmp;
