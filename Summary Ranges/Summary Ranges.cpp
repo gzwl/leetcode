0d253e74ef4eb13fb485db7a37d328ec1f46ca7a
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-06-27 23:00:11 +0800

    Implement

---
+++ Summary Ranges/Summary Ranges.cpp
@@ -0,0 +1,25 @@
+/*
+    做法很直接了，扫一遍就可以了
+    时间复杂度O(n)
+*/
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int lhs = 0,rhs = 0;
+        vector<string> res;
+        while(rhs < nums.size()){
+            while(rhs < nums.size() && nums[rhs] - nums[lhs] == rhs - lhs)  rhs++;
+            char tmp[100] = {0};
+            if(lhs == rhs - 1)  sprintf(tmp,"%d",nums[lhs]);
+            else    sprintf(tmp,"%d->%d",nums[lhs],nums[rhs - 1]);
+            string s;
+            int cnt = 0;
+            while(tmp[cnt]) s += tmp[cnt++];
+            lhs = rhs;
+            res.push_back(s);
+        }
+        return res;
+    }
+};
+
