7af0bc9e8c5d0022e6c0a2d72c84fba4e78b03ac
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-08-16 23:50:26 +0800

    Implementation

--- Binary Tree Paths/Binary Tree Paths.cpp
+++ Binary Tree Paths/Binary Tree Paths.cpp
@@ -7,6 +7,10 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+ /*
+    直接dfs就可以了，把数转化为字符串的时候要注意数不只有一位并且还可能有负数
+*/
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
@@ -17,17 +21,31 @@ public:
         return res;
     }
     void dfs(vector<string>& res,string s,TreeNode* ptr){
+        if(ptr->val == 0)   s += '0';
+        else{
+            if(ptr->val < 0){
+                ptr->val *= -1;
+                s += '-';
+            }
+            string tmp;
+            while(ptr->val){
+                tmp += ((ptr->val % 10) + '0');
+                ptr->val /= 10;
+            }
+            reverse(tmp.begin(),tmp.end());
+            s += tmp;
+        }
         if(ptr->left == NULL && ptr->right == NULL){
-            s += (char)('0' + ptr->val);
-            res.push_back(res);
+            res.push_back(s);
             return ;
         }
         else{
-            s += (char)('0' + ptr->val);
-            s += '->';
+            s += '-';
+            s += '>';
             if(ptr->left)   dfs(res,s,ptr->left);
             if(ptr->right)      dfs(res,s,ptr->right);
         }
     }
 };
+
 
