a1731539918629697860a7fdef716b44061110a3
Weiliang Zhang <mrweiliangzhang@gmail.com>
2015-08-22 12:46:46 +0800

    Math,Dp

--- Ugly Number II/Ugly Number II.cpp
+++ Ugly Number II/Ugly Number II.cpp
@@ -1,21 +1,41 @@
+/*
+    首先，新的一个ugly number肯定是从较小的ugly number乘以2,3或5得来的
+    那么我们维护三个下标id0,id1,id
+    则ugly[id0]*2,ugly[id1]*3,ugly[id2]*5的最小值为下一个数
+    注意最小值必须大于当前已有ugly number中的最大值，不然就会重复了
+    time O(n)
+*/
+
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        --n;
-        int a = n / 3;
-        int b = n % 3;
-        int res = 1;
-        res *= pow(2,a) * pow(3,a) * pow(5,a);
-        if(b == 1)   res *= 2;
-        if(b == 2)   res *= 3;
+        typedef long long ll;
+        int *ugly = new int[n];
+        int id0 = 0,id1 = 0,id2 = 0;
+        int cnt = 0;
+        ugly[cnt] = 1;
+        while(cnt != n - 1){
+            //必须大于当前已有ugly number中的最大值，不然就会重复了
+            while(ugly[id0] * 2 <= ugly[cnt])   ++id0;
+            while(ugly[id1] * 3 <= ugly[cnt])   ++id1;
+            while(ugly[id2] * 5 <= ugly[cnt])   ++id2;
+            //最小值为下一个数
+            int minid = id0,minval = ugly[id0] * 2;
+            if(ugly[id1] * 3 < minval){
+                minval = ugly[id1] * 3;
+                minid = id1;
+            }
+            if(ugly[id2] * 5 < minval){
+                minval = ugly[id2] * 5;
+                minid = id2;
+            }
+            ugly[++cnt] = minval;
+            if(minid == id0)    ++id0;
+            else if(minid == id1)   ++id1;
+            else    ++id2;
+        }
+        int res = ugly[n-1];
+        delete [] ugly;
         return res;
     }
-    int pow(int x,int y)    //蹇�熷啣姹倄^y
-    {
-        if(y == 0)  return 1;
-        int tmp = pow(x,y/2);
-        if(y % 2)   return tmp * tmp * x;
-        else    return tmp * tmp;
-    }
 };
-
